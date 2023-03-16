fa18fe53ec27eee2b781f7e95df851478c16f483
isnendyan <isnendyankp@gmail.com>
2023-03-16 08:11:19 +0700

    add basic dark&light mode

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-    console.log(event);
+    if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+} else {
+    document.documentElement.setAttribute('data-theme', 'light');
   }
+}
   
   // Event Listener
   toggleSwitch.addEventListener('change', switchTheme);
