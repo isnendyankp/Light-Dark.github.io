97d4b664f2f6719ec25e8db4f758680a0b27d552
isnendyan <isnendyankp@gmail.com>
2023-03-17 08:10:29 +0700

    add dark mode at nav & change text content,symbol light into dark mode

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-    if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-} else {
-    document.documentElement.setAttribute('data-theme', 'light');
-  }
+	if (event.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+		darkMode();
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+		lightMode();
+	}
 }
-  
-  // Event Listener
-  toggleSwitch.addEventListener('change', switchTheme);
+
+// Dark Mode Styles
+function darkMode() {
+	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+	toggleIcon.children[0].textContent = 'Dark Mode';
+	toggleIcon.children[1].classList.remove('fa-sun');
+	toggleIcon.children[1].classList.add('fa-moon');
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
